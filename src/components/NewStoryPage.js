1eaf1df9685b69606d7c1903f82a05ea9ace5790
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-06-09 22:22:23 -0300

    Able to create new posts POSTing /posts

--- src/components/NewStoryPage.js
+++ src/components/NewStoryPage.js
@@ -1,27 +1,30 @@
-import { useState } from 'react';
-import axios from 'axios';
-import { useHistory } from 'react-router-dom';
-import PostManipulation from './PostManipulation/PostManipulation';
+import { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
+import PostManipulation from "./PostManipulation/PostManipulation";
 
 export default function NewStoryPage() {
-  const [title, setTitle] = useState('');
-  const [coverUrl, setCoverUrl] = useState('');
-  const [content, setContent] = useState('');
-  const [isSaveButtonDisabled, setSaveButtonDisable] = useState(false);
-  const history = useHistory();
+    const [title, setTitle] = useState("");
+    const [coverUrl, setCoverUrl] = useState("");
+    const [content, setContent] = useState("");
+    const [isSaveButtonDisabled, setSaveButtonDisable] = useState(false);
+    const history = useHistory();
 
-  function onPostSaveButtonClick() {}
-
-  return (
-    <PostManipulation
-      title={title}
-      onTitleChange={(newTitle) => setTitle(newTitle)}
-      coverUrl={coverUrl}
-      onCoverUrlChange={(newCoverUrl) => setCoverUrl(newCoverUrl)}
-      content={content}
-      onContentChange={(newContent) => setContent(newContent)}
-      onPostSaveButtonClick={onPostSaveButtonClick}
-      isSaveButtonDisabled={isSaveButtonDisabled}
-    />
-  );
+    function onPostSaveButtonClick() {
+        const body = { title, coverUrl, content };
+        const request = axios.post("http://localhost:4000/posts", body);
+        request.then(history.push("/"));
+    }
+    return (
+        <PostManipulation
+            title={title}
+            onTitleChange={(newTitle) => setTitle(newTitle)}
+            coverUrl={coverUrl}
+            onCoverUrlChange={(newCoverUrl) => setCoverUrl(newCoverUrl)}
+            content={content}
+            onContentChange={(newContent) => setContent(newContent)}
+            onPostSaveButtonClick={onPostSaveButtonClick}
+            isSaveButtonDisabled={isSaveButtonDisabled}
+        />
+    );
 }
